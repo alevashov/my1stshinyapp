9980bef2e5e22d9fd248927541fe2a1843590158
alevashov <alevashov@users.noreply.github.com>
2015-02-19 11:32:45 +1100

    Deleting temp file

--- tmp.R
+++
@@ -1,7 +0,0 @@
-pal <- brewer.pal(9,"BuGn")
-pal <- pal[-(1:4)]
-wordcloud(words=words, freq=freq, min.freq=1, max.words=200, colors=pal)
-renderPlot({
-        wordcloud(words=words, freq=freq, min.freq=1, max.words=200, 
-                  colors=pal)
-})
